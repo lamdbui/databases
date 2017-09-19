d162fe7beb1d400759ab4d65ca28a6e38c933f02
Hack Reactor Student <student@hackreactor.com>
2017-09-19 10:30:57 -0700

    Add get/post for users in database interface

--- server/models/index.js
+++ server/models/index.js
@@ -1,26 +1,38 @@
 var db = require('../db');
+var connection = db.connection;
 
 
 module.exports = {
   messages: {
     get: function () {
-      // res.send()
 
     }, // a function which produces all the messages
     post: function () {
-      res.send('hello');
 
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {
-
+    get: function (callback) {
+      connection.query('SELECT name FROM users', (error, results, fields) => {
+        let queryResults = [];
+        console.log(results);
+        results.forEach((row) => {
+          queryResults.push({'username': row.name});
+        });
+        callback(queryResults);
+      });
     },
-    post: function () {
-      res.send('hello');
+    post: function (username, callback) {
+      connection.query(`INSERT INTO users values(null, '${username}')`, (error, results, fields) => {
+        if (error) {
+          console.log('ERROR: problem inserting user -', username);
+          callback(false);
+        }
+        console.log(`SUCCESS: '${username}' successfully added into table`);
+        callback(true);
+      });
     }
   }
 };
-
