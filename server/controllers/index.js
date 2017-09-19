d3f0c6a60c82f8ca2c8a8cf836c287f86172b7f8
Hack Reactor Student <student@hackreactor.com>
2017-09-19 15:49:41 -0700

    Connect conroller post to database for messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,15 +2,28 @@ var models = require('../models');
 
 module.exports = {
   messages: {
+    // a function which handles a get request for all messages
     get: function (req, res) {
       models.messages.get(data => {
         res.statusCode = 200;
         res.send(data);
       });
-    }, // a function which handles a get request for all messages
+    },
+    // a function which handles posting a message to the database
     post: function (req, res) {
-
-    } // a function which handles posting a message to the database
+      var newMessage = req.body;
+      models.messages.post(newMessage, (postSucceeded) => {
+        if (postSucceeded) {
+          console.log('Successfully added message');
+          res.statusCode = 201;
+          res.send();
+        } else {
+          console.log('ERROR adding message');
+          res.statusCode = 400;
+          res.send();
+        }
+      });
+    }
   },
 
   users: {
