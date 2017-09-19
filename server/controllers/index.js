fc83a2fe04fbf106d8d7fc9123b0d31dfc3ce829
Hack Reactor Student <student@hackreactor.com>
2017-09-19 16:17:29 -0700

    Added status code for posts and get and functionality on user posts

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,21 +5,23 @@ module.exports = {
     // a function which handles a get request for all messages
     get: function (req, res) {
       models.messages.get(data => {
-        res.statusCode = 200;
+        res.status(200);
+        res.set('Content-Type', 'application/json');
         res.send(data);
       });
     },
     // a function which handles posting a message to the database
     post: function (req, res) {
       var newMessage = req.body;
-      models.messages.post(newMessage, (postSucceeded) => {
+      models.messages.post(newMessage, postSucceeded => {
         if (postSucceeded) {
           console.log('Successfully added message');
-          res.statusCode = 201;
+          res.status(201);
+          res.set('Content-Type', 'text/html');
           res.send();
         } else {
           console.log('ERROR adding message');
-          res.statusCode = 400;
+          res.status(400);
           res.send();
         }
       });
@@ -27,19 +29,29 @@ module.exports = {
   },
 
   users: {
-    // Ditto as above
+    // a function which handles a get request for all users
     get: function (req, res) {
       models.users.get(data => {
-        res.statusCode = 200;
+        res.status(200);
+        res.set('Content-Type', 'application/json');
         res.send(data);
       });
     },
+    // a function which handles posting a user to the database
     post: function (req, res) {
-      // req.body
-
-
-      res.send('hello');
-
+      var newUser = req.body;
+      models.users.post(newUser.username, postSucceeded => {
+        if (postSucceeded) {
+          console.log('Successfully added user');
+          res.status(201);
+          res.set('Content-Type', 'text/html');
+          res.send();
+        } else {
+          console.log('ERROR adding user');
+          res.status(400);
+          res.send();
+        }
+      });
     }
   }
 };
