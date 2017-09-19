53631275db9f52a5a4895a7bd1ff2991a4a7e3d7
Hack Reactor Student <student@hackreactor.com>
2017-09-19 11:13:21 -0700

    Fully implement post for messages for database interface

--- server/models/index.js
+++ server/models/index.js
@@ -4,61 +4,69 @@ var connection = db.connection;
 
 module.exports = {
   messages: {
+    // a function which produces all the messages
     get: function (callback) {
 
-    }, // a function which produces all the messages
+    },
+    // a function which can be used to insert a message into the database
     post: function (message, callback) {
-      // TODO: create room entry, if does not exist
-
-      // if  length of (select * from rooms where rooms.name=message.roomname) === 0
-        // create our room and add to rooms table
-
-      connection.query(`SELECT * from rooms WHERE name='${message.roomname}'`, (error, results, fields) => {
+      connection.query(`SELECT * from rooms WHERE name='${message.roomname}'`, (error, roomResults, fields) => {
         if (error) {
           console.log('ERROR: problem accessing rooms table');
-          // console.log('ERROR: problem creating room -', message.roomname);
-          //callback(false);
+          callback(false);
+          return;
         }
-        if (results.length === 0) {
+        // checking for existence for room
+        if (roomResults.length === 0) {
           connection.query(`INSERT INTO rooms VALUES(null, '${message.roomname}')`, (error, results, fields) => {
             if (error) {
               console.log('ERROR: problem creating room -', message.roomname);
-              //callback(false);
+              callback(false);
             } else {
-              console.log('room results:', results);
-              connection.query(`INSERT INTO messages VALUES(null, '${message.username}', '${message.message}', '${message.roomname}')`, (error, results, fields) => {
+              connection.query(`SELECT * from users WHERE name='${message.username}'`, (error, userResults, fields) => {
                 if (error) {
-                  console.log('ERROR: problem inserting message -', message, 'with error:', error);
+                  console.log('ERROR: problem accessing users table');
                   callback(false);
+                } else {
+                  // checking for existence for user
+                  if (userResults.length === 1) {
+                    connection.query(`INSERT INTO messages VALUES(null, ${userResults[0].id}, '${message.message}', ${roomResults[0].id})`, (error, results, fields) => {
+                      if (error) {
+                        console.log('ERROR: problem inserting message -', message, 'with error:', error);
+                        callback(false);
+                      } else {
+                        console.log(`SUCCESS: '${message}' successfully added into table`);
+                        callback(true);
+                      }
+                    });
+                  }
                 }
-                console.log(`SUCCESS: '${message}' successfully added into table`);
-                callback(true);
               });
             }
           });
         } else {
-          connection.query(`INSERT INTO messages VALUES(null, '${message.username}', '${message.message}', '${message.roomname}')`, (error, results, fields) => {
+          connection.query(`SELECT * from users WHERE name='${message.username}'`, (error, userResults, fields) => {
             if (error) {
-              console.log('ERROR: problem inserting message -', message, 'with error2:', error);
+              console.log('ERROR: problem accessing users table');
               callback(false);
+            } else {
+              // checking for existence for user
+              if (userResults.length === 1) {
+                connection.query(`INSERT INTO messages VALUES(null, ${userResults[0].id}, '${message.message}', ${roomResults[0].id})`, (error, results, fields) => {
+                  if (error) {
+                    console.log('ERROR: problem inserting message -', message, 'with error:', error);
+                    callback(false);
+                  } else {
+                    console.log(`SUCCESS: '${message}' successfully added into table`);
+                    callback(true);
+                  }
+                });
+              }
             }
-            console.log(`SUCCESS: '${message}' successfully added into table`);
-            callback(true);
           });
         }
-        // console.log(`SUCCESS: '${message.roomname}' successfully added into table`);
-        //callback(true);
       });
-
-      // connection.query(`INSERT INTO messages VALUES(null, '${message.username}', '${message.message}', '${message.roomname}')`, (error, results, fields) => {
-      //   if (error) {
-      //     console.log('ERROR: problem inserting message -', message);
-      //     callback(false);
-      //   }
-      //   console.log(`SUCCESS: '${message}' successfully added into table`);
-      //   callback(true);
-      // });
-    } // a function which can be used to insert a message into the database
+    }
   },
 
   users: {
