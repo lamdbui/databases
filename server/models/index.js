7eb2a25476842c83feae049e93f8bec4f18c46ef
Hack Reactor Student <student@hackreactor.com>
2017-09-19 15:06:55 -0700

    Implement get functionality for messages on database

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,55 @@ module.exports = {
   messages: {
     // a function which produces all the messages
     get: function (callback) {
+      // query into messages and grab all messages.
+      let results = [];
+      connection.query('SELECT * from messages', (error, messageResults, fields) => {
+        if (error) {
+          console.log('ERROR: problem accessing messages', error);
+          callback(JSON.stringify(results));
+        } else {
+          // for each message in messageResults
+          messageResults.forEach(message => {
+            // convert message into result message format
+            let messageText = message.message_text;
+            connection.query(`SELECT name from rooms WHERE id=${message.room_id}`, (error, roomResults, fields) => {
+              if (error) {
+                console.log('Error accessing rooms', error);
+                callback(JSON.stringify(results));
+              } else {
+                // checking if room exists
+                if (roomResults.length === 1) {
+                  // convert user into result message format
+                  connection.query(`SELECT name from users WHERE id=${message.username_id}`, (error, userResults, fields) => {
+                    if (error) {
+                      console.log('Error accessing users', error);
+                      callback(JSON.stringify(results));
+                    } else {
+                      // if user exists
+                      if (userResults.length === 1) {
+                        // create message object with the roomname, username and text
+                        let message = {
+                          roomname: roomResults[0].name,
+                          username: userResults[0].name,
+                          text: messageText
+                        };
 
+                        results.push(message);
+                        if (messageResults.length === results.length) {
+                          // returns an array of message objects with fields of username, text, roomname, serialized to a json string
+                          callback(JSON.stringify(results));
+                        }
+                      }
+                    }
+                  });
+                } else {
+                  // TODO: handle case when there are no matching room names for id
+                }
+              }
+            });
+          });
+        }
+      });
     },
     // a function which can be used to insert a message into the database
     post: function (message, callback) {
